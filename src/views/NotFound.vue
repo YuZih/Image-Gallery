57ffbeab8a1a06f0af6e0e4253b06bdf5cbea5bb
Yuzu <yuzuprocoding@gmail.com>
2023-03-30 00:14:42 +0800

    add NotFound page, setups scss file

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>404 Page Not Found</h1>
+    <h1>中文字體</h1>
+    <h1>English </h1>
+    <h1>12334</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
