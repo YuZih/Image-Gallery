b17b47b0fe55bd654d4e78da8af573fa0b939a3e
Yuzu <yuzuprocoding@gmail.com>
2023-03-31 01:49:01 +0800

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <Header/>
     <router-view />
   </div>
 </template>
@@ -12,8 +9,13 @@
 <script>
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import Header from '@/components/Header.vue'
+
 export default {
   name: "App",
+  components: {
+    Header
+  }
 };
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <header>
+    <div class="h container">
+      <div class="h__brand">Yuzu x Pic</div>
+
+      <div class="h__navbar nav-justified">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">POST</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+              aria-expanded="false">Dropdown</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Wedding-1</a></li>
+              <li><a class="dropdown-item" href="#">Wedding-2</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Vintage-1</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">ABOUT</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style lang="scss">
+.h {
+  // max-width: 768px;
+  text-align: center;
+  border: 1px green solid;
+
+  &__brand {
+    font-family: 'Dancing Script', cursive;
+    font-size: 30px;
+    font-weight: 500;
+    margin: 30px 0 30px 0;
+    border: 1px blue solid;
+  }
+
+  &__navbar {
+    font-size: 14px;
+    border: 1px red solid;
+
+  }
+}
+</style>
