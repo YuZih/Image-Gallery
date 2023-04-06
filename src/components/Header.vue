5b95fbe53511b8ef4894d807d37a52d8ae0fdad8
Yuzu <yuzuprocoding@gmail.com>
2023-04-07 01:48:00 +0800

    add navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,34 +1,65 @@
 <template>
-  <header>
-    <div class="h container">
-      <div class="h__brand">Yuzu x Pic</div>
+  <header class="container-fluid">
 
-      <div class="h__navbar nav-justified">
-        <ul class="nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">HOME</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">POST</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
-              aria-expanded="false">ALBUM</a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Wedding-1</a></li>
-              <li><a class="dropdown-item" href="#">Wedding-2</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Vintage-1</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link">ABOUT</a>
-          </li>
-        </ul>
-      </div>
+    <!-- logo -->
+    <div class="logo text-center mt-4 my-md-4">
+      <a href="#">Yuzu x Pic</a>
     </div>
+
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-md navbar-light">
+      <div class="container-fluid justify-content-center">
+
+        <!-- hamburger icon -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar1"
+          aria-controls="navbar1" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- nav tabs -->
+        <div class="collapse navbar-collapse" id="navbar1">
+          <ul class="navbar-nav me-auto justify-content-evenly w-100">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">POST</a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                ALBUM
+              </a>
+
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <div class="dropdown-header">Wedding Series</div>
+                </li>
+                <li><a class="dropdown-item" href="#">Wedding-1</a></li>
+                <li><a class="dropdown-item" href="#">Wedding-2</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+
+                <li>
+                  <div class="dropdown-header">Vintage Series</div>
+                </li>
+                <li><a class="dropdown-item" href="#">Vintage-1</a></li>
+                <li><a class="dropdown-item" href="#">Vintage-2</a></li>
+              </ul>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link">ABOUT</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <hr>
   </header>
 </template>
 
@@ -39,38 +70,87 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.h {
-  max-width: 768px;
-  text-align: center;
-  border: 1px green solid;
+@use "@/assets/scss/setups.scss";
 
-  &__brand {
-    font-family: 'Dancing Script', cursive;
-    font-size: 30px;
-    font-weight: 500;
-    margin: 30px 0 30px 0;
-    border: 1px blue solid;
-  }
+// XS screen
+header {
+  position: relative;
 
-  &__navbar {
-    font-size: 14px;
-    border: 1px red solid;
-
+  .navbar-toggler {
+    position: absolute;
+    top: -35px;
+    left: 0px;
+    border: none;
   }
 }
 
-.dropdown {
-  .dropdown-menu {
-    display: none;
-  }
+.logo {
+  @extend %logo-font;
+  font-size: 28px;
+  font-weight: 600;
+  border: 1px red solid;
 
-  &:hover {
+  :hover {
+    cursor: pointer;
+  }
+}
+
+#navbar1 {
+  border: 1px blue solid;
+  max-width: 800px;
+
+  .navbar-nav {
+    text-align: center;
+
+    .nav-item {
+      padding: 1rem;
+    }
+
     .dropdown-menu {
-      display: block;
+      line-height: 1.5rem;
+      text-align: center;
+
+      .dropdown-header {
+        font-style: italic;
+      }
+
+      .dropdown-divider {
+        display: none;
+      }
     }
   }
 }
 
-//dropdown-toggle
-@media screen and (min-width: 768px) {}
+
+// Small screen: mobile
+@media screen and (min-width: 576px) {
+  .logo {
+    font-size: 34px;
+  }
+}
+
+
+// medium screen: iPad, PC
+@media screen and (min-width: 768px) {
+  .logo {
+    font-size: 40px;
+  }
+
+  //show dropdown menu when mouse hovers
+  .dropdown {
+    .dropdown-menu {
+      display: none;
+
+      .dropdown-divider {
+        display: block;
+      }
+    }
+
+    &:hover {
+      .dropdown-menu {
+        display: block;
+      }
+    }
+  }
+}
 </style>
